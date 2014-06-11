e8082430f2052dc5bdbdd47c532bfeda0d4d087f
nogradiadam <nogradiadam@gmail.com>
2014-06-11 23:36:52 +0100

    Adding GO Encyclopaedia

---
+++ src/cs/man/ac/uk/encyclopaedia/client/OntologyEncyclopaedia.java
@@ -0,0 +1,23 @@
+package cs.man.ac.uk.encyclopaedia.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class OntologyEncyclopaedia implements EntryPoint {
+	
+
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 */
+	private final GreetingServiceAsync greetingService = GWT
+			.create(GreetingService.class);
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
