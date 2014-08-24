8c1b9b6a65bac99878174f08f6bd33eded75f052
nogradiadam <nogradiadam@gmail.com>
2014-08-24 23:33:36 +0200

    Class labels with definitions working - needs refactoring

--- src/cs/man/ac/uk/encyclopaedia/server/OntologyExtractor.java
+++ src/cs/man/ac/uk/encyclopaedia/server/OntologyExtractor.java
@@ -21,7 +21,6 @@ import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
 
 import uk.ac.manchester.cs.owl.owlapi.OWLAnonymousIndividualImpl;
 
@@ -65,48 +64,45 @@ public class OntologyExtractor {
 		return nonObsoleteList;
 	}
 	
-	//TODO delete this method once implemented elsewhere. Create Mapping of OWL class and its name and def
-		private List<String> getClassNames (List<OWLClass> classes) {
-			List<String> classNamesList = new ArrayList<String>();
-
-			OWLAnnotationProperty label = df.getOWLAnnotationProperty(OWLRDFVocabulary.RDFS_CLASS.getIRI());
-
-			for (OWLClass cls : classes) {
-				for (OWLAnnotation annotation : cls.getAnnotations(ont, label)) {
-					if (annotation.getValue() instanceof OWLLiteral) {
-						OWLLiteral val = (OWLLiteral) annotation.getValue();
-						classNamesList.add(val.toString());
-					}
-				}
-			}
-			return classNamesList;
-		}
+	//TODO move label finding bit here
+	//TODO: move definition finding bit here
 
 	public Map<String, String> getClassesWithDefinitions (List<OWLClass> classes){
-		List<String> classNames = getClassNames(classes);
 		OWLAnnotationProperty hasDefinition = df.getOWLAnnotationProperty(IRI.create("http://www.geneontology.org/formats/oboInOwl#hasDefinition"));
 		OWLAnnotationProperty rdfsLabel = df.getRDFSLabel();
 		Map<String, String> classesWithDefs = new TreeMap <String, String> ();
+		Map<String, String> classesWithLabels = new TreeMap <String, String> ();
 		
-		int classIndex = 0;
 
 		for(OWLClass cl:classes) {
-			Set<OWLAnnotation> annos = cl.getAnnotations(ont, hasDefinition);
-			for(OWLAnnotation anno:annos) {
+			Set<OWLAnnotation> definition = cl.getAnnotations(ont, hasDefinition);
+			for(OWLAnnotation anno:definition) {
 				OWLAnnotationValue value = anno.getValue();
 				if(value instanceof OWLAnonymousIndividualImpl) {
 					OWLAnonymousIndividual i = (OWLAnonymousIndividualImpl)value;
 					for(OWLAnnotationAssertionAxiom ax:ont.getAnnotationAssertionAxioms(i)) {
 						if(ax.getProperty().equals(rdfsLabel)) {
 							OWLAnnotationValue av = ax.getValue();
-							// TODO cl.toString() gives the class URI. Need to extract the name literal from it
 							classesWithDefs.put(cl.toString(), av.toString());
-							classIndex++;
 						}
 					}
 				}
 			}
+			
+			for (OWLAnnotation annotation : cl.getAnnotations(ont, df.getRDFSLabel())) {
+				if (annotation.getValue() instanceof OWLLiteral) {
+				    OWLLiteral val = (OWLLiteral) annotation.getValue();
+				    classesWithLabels.put(cl.toString(), val.getLiteral());
+				   }
+			}
 		}
-		return classesWithDefs;
+		
+		Map<String, String> labelsWithDefs = new TreeMap <String, String> ();
+		// TODO: if key is not found this'll throw an exception
+		for (String key : classesWithDefs.keySet()) {
+			labelsWithDefs.put(classesWithLabels.get(key), classesWithDefs.get(key));
+		}
+		
+		return labelsWithDefs;
 	}
 }
